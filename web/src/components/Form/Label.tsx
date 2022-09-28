ff87339a4eff54f634922895bc8e46546f566b98
Fellipe Utaka <fellipeutaka@gmail.com>
2022-09-28 08:02:43 -0300

    Revert "feat: react-hook-form"
    This reverts commit fdf1c7a02461fe1acd8791102c7c48298bfe13e8.

--- web/src/components/Form/Label.tsx
+++ web/src/components/Form/Label.tsx
@@ -3,5 +3,5 @@ import { HTMLProps } from "react";
 interface LabelProps extends HTMLProps<HTMLLabelElement> {}
 
 export function Label(props: LabelProps) {
-  return <label {...props} className={`font-semibold ${props.className}`} />;
+  return <label className="font-semibold" {...props} />;
 }
--- web/src/components/Form/Select.tsx
+++ web/src/components/Form/Select.tsx
@@ -1,11 +1,21 @@
-import { useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
 import { GameAd } from "@@types/GameAd";
 import * as SelectPrimitive from "@radix-ui/react-select";
 import styles from "@styles/select.module.css";
 import { CaretDown, CaretUp, Check } from "phosphor-react";
 
-export function Select() {
+import { Fields } from "@components/CreateAd/Dialog";
+
+const SelectPlaceholder = (
+  <span className="text-zinc-500">Selecione o game que deseja jogar</span>
+);
+
+interface SelectProps {
+  setFields: Dispatch<SetStateAction<Fields>>;
+}
+
+export function Select({ setFields }: SelectProps) {
   const [gameAds, setGameAds] = useState<GameAd[]>([]);
 
   useEffect(() => {
@@ -15,29 +25,44 @@ export function Select() {
   }, []);
 
   return (
-    <SelectPrimitive.Portal>
-      <SelectPrimitive.Content className={styles.content}>
-        <SelectPrimitive.ScrollUpButton className="flex justify-center items-center h-8">
-          <CaretUp />
-        </SelectPrimitive.ScrollUpButton>
-        <SelectPrimitive.Viewport className="p-2">
-          {gameAds.map((ad) => (
-            <SelectPrimitive.Item
-              key={ad.id}
-              value={ad.id}
-              className={styles.item}
-            >
-              <SelectPrimitive.ItemText>{ad.name}</SelectPrimitive.ItemText>
-              <SelectPrimitive.ItemIndicator className="absolute left-0 w-6 flex items-center justify-center">
-                <Check />
-              </SelectPrimitive.ItemIndicator>
-            </SelectPrimitive.Item>
-          ))}
-        </SelectPrimitive.Viewport>
-        <SelectPrimitive.ScrollDownButton className="flex justify-center items-center h-8">
-          <CaretDown />
-        </SelectPrimitive.ScrollDownButton>
-      </SelectPrimitive.Content>
-    </SelectPrimitive.Portal>
+    <SelectPrimitive.Root
+      name="game"
+      onValueChange={(game) => setFields((state) => ({ ...state, game }))}
+    >
+      <SelectPrimitive.Trigger
+        id="game"
+        className="flex items-center justify-between bg-zinc-900 py-3 px-4 rounded text-sm outline focus:outline-violet-500"
+      >
+        <SelectPrimitive.Value placeholder={SelectPlaceholder} />
+        <SelectPrimitive.Icon>
+          <CaretDown size={24} className="text-zinc-400" />
+        </SelectPrimitive.Icon>
+      </SelectPrimitive.Trigger>
+
+      <SelectPrimitive.Portal>
+        <SelectPrimitive.Content className={styles.content}>
+          <SelectPrimitive.ScrollUpButton className="flex justify-center items-center h-8">
+            <CaretUp />
+          </SelectPrimitive.ScrollUpButton>
+          <SelectPrimitive.Viewport className="p-2">
+            {gameAds.map((ad) => (
+              <SelectPrimitive.Item
+                key={ad.id}
+                value={ad.id}
+                className={styles.item}
+              >
+                <SelectPrimitive.ItemText>{ad.name}</SelectPrimitive.ItemText>
+                <SelectPrimitive.ItemIndicator className="absolute left-0 w-6 flex items-center justify-center">
+                  <Check />
+                </SelectPrimitive.ItemIndicator>
+              </SelectPrimitive.Item>
+            ))}
+          </SelectPrimitive.Viewport>
+          <SelectPrimitive.ScrollDownButton className="flex justify-center items-center h-8">
+            <CaretDown />
+          </SelectPrimitive.ScrollDownButton>
+        </SelectPrimitive.Content>
+      </SelectPrimitive.Portal>
+    </SelectPrimitive.Root>
   );
 }
