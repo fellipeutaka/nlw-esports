2244525feca3037e5dcf4ec8d02d68a61dd8ba57
Fellipe Utaka <fellipeutaka@gmail.com>
2022-09-28 08:02:43 -0300

    Revert "feat: discord oauth"
    This reverts commit a847e9f274f85ca936adb4f5a14ef9ae3756bd11.

--- mobile/src/@types/routes/NavigationProps/Auth.ts
+++
@@ -1,6 +0,0 @@
-import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-
-import { AuthStackParamsList } from "../ParamsList/Auth";
-
-export type AuthStackNavigationProps =
-  NativeStackNavigationProp<AuthStackParamsList>;
